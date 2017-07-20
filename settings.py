aa9c7fb4c1b3496f9df0e29ce767960925f01bc5
Eduardo Sebastian <esebastian@altiora.es>
2017-07-20 12:21:14 +0200

    Update stats data

--- settings.py
+++ settings.py
@@ -93,10 +93,10 @@ ANALYTICS_ID            = ''
 DATA_SOURCE_BUDGET      = ''
 
 # Setup Data Source Population link
-DATA_SOURCE_POPULATION  = ''
+DATA_SOURCE_POPULATION  = 'http://www.ine.es/jaxiT3/Tabla.htm?t=2905&L=0'
 
 # Setup Data Source Inflation link
-DATA_SOURCE_INFLATION   = ''
+DATA_SOURCE_INFLATION   = 'http://www.ine.es/jaxiT3/Tabla.htm?t=22350&L=0'
 
 # Setup Main Entity Web Url
 MAIN_ENTITY_WEB_URL     = ''
